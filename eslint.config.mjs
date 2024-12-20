64ec9475c8b0d7be475d6e8e1e1e11e7247939e0
Tim Rogers <me@timrogers.co.uk>
2024-12-20 13:29:31 +0000

    Migrate ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import prettier from "eslint-plugin-prettier";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [
+    ...compat.extends("prettier", "eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    {
+        plugins: {
+            prettier,
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/strict-boolean-expressions": "off",
+            "prettier/prettier": "error",
+        },
+    },
+];
